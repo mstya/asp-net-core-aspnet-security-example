e5d0c56f27eec54548dc4b221b7c9c7182537a21
Viacheslav Yankov <slava.brigida@nixsolutions.com>
2017-10-22 20:53:39 +0300

    moved auth to authprovider

---
+++ AspNetSecurityIntro/Controllers/AuthorizationController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AspNet.Security.OpenIdConnect.Extensions;
+using AspNet.Security.OpenIdConnect.Primitives;
+using AspNet.Security.OpenIdConnect.Server;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetSecurityIntro.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        [Authorize, HttpGet("~/connect/token")]
+        public Task GetAccessToken(HandleTokenRequestContext context, CancellationToken cancellationToken)
+        {
+            if (context.Request.IsPasswordGrantType())
+            {
+                if (!string.Equals(context.Request.Username, "Bob", StringComparison.Ordinal) ||
+                    !string.Equals(context.Request.Password, "P@ssw0rd", StringComparison.Ordinal))
+                {
+                    context.Reject(
+                        error: OpenIdConnectConstants.Errors.InvalidGrant,
+                        description: "Invalid user credentials.");
+
+                    return Task.CompletedTask;
+                }
+
+                var identity = new ClaimsIdentity(context.Scheme.Name,
+                    OpenIdConnectConstants.Claims.Name,
+                    OpenIdConnectConstants.Claims.Role);
+
+                identity.AddClaim(OpenIdConnectConstants.Claims.Subject, "[unique id]", OpenIdConnectConstants.Destinations.IdentityToken);
+
+                identity.AddClaim("urn:customclaim", "value",
+                    OpenIdConnectConstants.Destinations.AccessToken,
+                    OpenIdConnectConstants.Destinations.IdentityToken);
+
+                var ticket = new AuthenticationTicket(
+                    new ClaimsPrincipal(identity),
+                    new AuthenticationProperties(),
+                    context.Scheme.Name);
+                ticket.SetResources("resource_server");
+                ticket.SetScopes(
+                    OpenIdConnectConstants.Scopes.Profile,
+                    OpenIdConnectConstants.Scopes.OfflineAccess
+                );
+
+                context.Validate(ticket);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- AspNetSecurityIntro/Startup.cs
+++ AspNetSecurityIntro/Startup.cs
@@ -40,88 +40,15 @@ namespace AspNetSecurityIntro
 
             services.AddAuthentication().AddOpenIdConnectServer(options =>
             {
-                options.SystemClock = new SystemClock();
+                options.ProviderType = typeof(AuthorizationProvider);
+
                 options.TokenEndpointPath = "/connect/token";
                 options.IntrospectionEndpointPath = "/connect/introspect";
                 options.AllowInsecureHttp = true;
                 options.AccessTokenLifetime = TimeSpan.FromDays(365);
-                
-                options.Issuer = new Uri("http://localhost:64855");
-                options.ApplicationCanDisplayErrors = true;
-
-                options.Provider.OnValidateIntrospectionRequest = context =>
-                {
-                    if (string.Equals(context.ClientId, "client_id", StringComparison.Ordinal) &&
-                        string.Equals(context.ClientSecret, "client_secret", StringComparison.Ordinal))
-                    {
-                        context.Validate();
-                    }
-
-                    return Task.CompletedTask;
-                };
-
-                options.Provider.OnValidateTokenRequest = context =>
-                {
-                    // Reject token requests that don't use grant_type=password or grant_type=refresh_token.
-                    if (!context.Request.IsPasswordGrantType() && !context.Request.IsRefreshTokenGrantType())
-                    {
-                        context.Reject(
-                            error: OpenIdConnectConstants.Errors.UnsupportedGrantType,
-                            description: "Only grant_type=password and refresh_token " +
-                                         "requests are accepted by this server.");
-
-                        return Task.CompletedTask;
-                    }
-
-                    if (string.Equals(context.ClientId, "client_id", StringComparison.Ordinal) &&
-                        string.Equals(context.ClientSecret, "client_secret", StringComparison.Ordinal))
-                    {
-                        context.Validate();
-                    }
-
-                    return Task.CompletedTask;
-                };
-
-                options.Provider.OnHandleTokenRequest = context =>
-                {
-                    if (context.Request.IsPasswordGrantType())
-                    {
-                        if (!string.Equals(context.Request.Username, "Bob", StringComparison.Ordinal) ||
-                            !string.Equals(context.Request.Password, "P@ssw0rd", StringComparison.Ordinal))
-                        {
-                            context.Reject(
-                                error: OpenIdConnectConstants.Errors.InvalidGrant,
-                                description: "Invalid user credentials.");
-
-                            return Task.CompletedTask;
-                        }
-
-                        var identity = new ClaimsIdentity(context.Scheme.Name,
-                            OpenIdConnectConstants.Claims.Name,
-                            OpenIdConnectConstants.Claims.Role);
-                        
-                        identity.AddClaim(OpenIdConnectConstants.Claims.Subject, "[unique id]", OpenIdConnectConstants.Destinations.IdentityToken);
-
-                        identity.AddClaim("urn:customclaim", "value",
-                            OpenIdConnectConstants.Destinations.AccessToken,
-                            OpenIdConnectConstants.Destinations.IdentityToken);
-                        
-                        var ticket = new AuthenticationTicket(
-                            new ClaimsPrincipal(identity),
-                            new AuthenticationProperties(),
-                            context.Scheme.Name);
-                        ticket.SetResources("resource_server");
-                        ticket.SetScopes(
-                            OpenIdConnectConstants.Scopes.Profile,
-                            OpenIdConnectConstants.Scopes.OfflineAccess
-                            );
-
-                        context.Validate(ticket);
-                    }
-
-                    return Task.CompletedTask;
-                };
             });
+
+            services.AddScoped<AuthorizationProvider>();
 
             services.AddMvc();
         }
